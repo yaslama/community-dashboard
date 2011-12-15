6171f5977f9c6f3021542c3bfb22f3976e93144b
brianleroux <brian@westcoastlogic.com>
2011-12-14 22:27:32 -0800

    use process port and default to 3000

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('coffee-script')
 var app = require('./routes')
-app.listen(3000);
-console.log("Express server listening on port %d", app.address().port);
+app.listen(process.env.PORT || 3000)
+console.log("Express server listening on port %d", app.address().port)
